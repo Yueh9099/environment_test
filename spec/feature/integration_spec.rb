4b035f6d3a78a01939b475fdb3c44024260ac3ed
root <root@localhost>
2021-09-29 20:09:17 +0000

    Add author, price and published_date

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -16,3 +16,54 @@ RSpec.describe 'Creating a book', type: :feature do
     expect(page).to have_content('Date: 2015-12-08')
   end
 end
+
+RSpec.describe 'Deleting a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Yue Hu'
+    fill_in 'Price', with: 2000
+    fill_in 'Published Date', with: Date.parse("Dec 8 2015")
+    click_on 'Create Book'
+    visit books_path
+    click_on 'Destory'
+    click_on 'OK'
+    visit books_path
+    expect(page).to have_content('Book was successfully destroyed.')
+  end
+end
+
+RSpec.describe 'Updating a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Yue Hu'
+    fill_in 'Price', with: 2000
+    fill_in 'Published Date', with: Date.parse("Dec 8 2015")
+    click_on 'Create Book'
+    visit books_path
+    click_on 'Edit'
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Jasson Hu'
+    fill_in 'Price', with: 2000
+    fill_in 'Published Date', with: Date.parse("Dec 8 2015")
+    click_on 'Update Book'
+    visit books_path
+    expect(page).to have_content('Jasson Hu')
+  end
+end
+
+RSpec.describe 'Showing a book', type: :feature do
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Yue Hu'
+    fill_in 'Price', with: 2000
+    fill_in 'Published Date', with: Date.parse("Dec 8 2015")
+    click_on 'Create Book'
+    visit books_path
+    click_on 'Show'
+    expect(page).to have_content('harry potter')
+  end
+end
+
